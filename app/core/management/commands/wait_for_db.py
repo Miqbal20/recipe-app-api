508d0f24910dce2809da0bc21df869a921c6f083
Muhammad Iqbal <miqbal020@gmail.com>
2024-01-30 00:39:40 +0700

    Setting Docker, Posgresql, Fix db race condition

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(self.style.WARNING('Waiting for database...'))
         db_up = False
-        while db_up == False:
+        while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
